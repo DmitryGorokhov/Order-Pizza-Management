100e68f8cb4602f1c1be0284ae4d758bdbfef041
Dmitrii Gorokhov <dgorohov80@gmail.com>
2020-11-09 15:25:31 +0300

    create project & create main menu

---
+++ Order-Pizza-Management/MainWindow.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Order_Pizza_Management
+{
+    /// <summary>
+    /// Логика взаимодействия для MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void CloseButton_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Grid_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void OrderButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentGrid.Children.Clear();
+            ContentGrid.Children.Add(new UserControlNewOrder());
+        }
+
+        private void ContolButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentGrid.Children.Clear();
+            // ContentGrid.Children.Add(new UserControlManage());
+        }
+
+        private void ReportButton_Click(object sender, RoutedEventArgs e)
+        {
+            ContentGrid.Children.Clear();
+            //ContentGrid.Children.Add(new UserControlReport());
+        }
+    }
+}
---
+++ Order-Pizza-Management/UserControlNewOrder.xaml.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Order_Pizza_Management
+{
+    /// <summary>
+    /// Логика взаимодействия для UserControlNewOrder.xaml
+    /// </summary>
+    public partial class UserControlNewOrder : UserControl
+    {
+        public UserControlNewOrder()
+        {
+            InitializeComponent();
+        }
+
+        private void MenuButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void CustomButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void FinishButton_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+    }
+}
